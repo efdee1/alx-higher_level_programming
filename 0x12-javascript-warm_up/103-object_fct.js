1fd17c19c8885e3329e472386202cf7b90bc79f6
Efdee1 <feranmi.adejinmi@gmail.com>
2022-09-05 18:49:06 -0700

    task103

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,20 +1,11 @@
 #!/usr/bin/node
 const myObject = {
-<<<<<<< HEAD
 	type: 'object',
 	value: 12
 };
 console.log(myObject);
 myObject.incr = function () {
 	this.value++;
-=======
-  type: 'object',
-  value: 12
-};
-console.log(myObject);
-myObject.incr = function () {
-  this.value++;
->>>>>>> db1c30065267d2bd5ab0ea98358626eeeb705096
 };
 myObject.incr();
 console.log(myObject);
